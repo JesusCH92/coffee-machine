04bd1287b8a6713212e5a58b3612e6466e996adf
JesusCH92 <jcocaharo@hotmail.com>
2021-03-15 20:48:39 +0100

    creating DTO for OrderDrink service

---
+++ src/Drink/ApplicationService/DTO/OrderDrinkRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GetWith\CoffeeMachine\Drink\ApplicationService\DTO;
+
+final class OrderDrinkRequest
+{
+    private string $drinkType;
+    private float $money;
+    private int $sugars;
+    private bool $extraHot;
+
+    public function __construct(
+        string $drinkType,
+        float $money,
+        int $sugars,
+        bool $extraHot
+    )
+    {
+        $this->drinkType = $drinkType;
+        $this->money = $money;
+        $this->sugars = $sugars;
+        $this->extraHot = $extraHot;
+    }
+
+    public function drinkType(): string
+    {
+        return $this->drinkType;
+    }
+
+    public function money(): float
+    {
+        return $this->money;
+    }
+
+    public function sugars(): int
+    {
+        return $this->sugars;
+    }
+
+    public function extraHot(): bool
+    {
+        return $this->extraHot;
+    }
+}
