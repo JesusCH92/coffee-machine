a3a95c8a07e95cc9702c4d5ea7630279adce47e2
JesusCH92 <jcocaharo@hotmail.com>
2021-03-15 22:05:18 +0100

    fix typo-> deleting quotation marks

--- src/Drink/Domain/Exception/PriceIsNotEnough.php
+++ src/Drink/Domain/Exception/PriceIsNotEnough.php
@@ -12,7 +12,7 @@ final class PriceIsNotEnough extends Exception
     public function __construct(string $drink, string $price, $code = 0, Throwable $previous = null)
     {
         parent::__construct(
-            sprintf('The "%s" costs "%s".', $drink, $price),
+            sprintf('The %s costs %s.', $drink, $price),
             $code,
             $previous
         );
