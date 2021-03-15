b97b8afeb598e3ee97f94bdbd2d8f7e024ba64de
JesusCH92 <jcocaharo@hotmail.com>
2021-03-15 20:53:07 +0100

    refactoring command-> executing service for order a drink

--- src/Console/MakeDrinkCommand.php
+++ src/Console/MakeDrinkCommand.php
@@ -2,6 +2,8 @@
 
 namespace GetWith\CoffeeMachine\Console;
 
+use GetWith\CoffeeMachine\Drink\ApplicationService\OrderDrink;
+use GetWith\CoffeeMachine\Drink\ApplicationService\DTO\OrderDrinkRequest;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -49,55 +51,18 @@ class MakeDrinkCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $drinkType = strtolower($input->getArgument('drink-type'));
-        if (!in_array($drinkType, ['tea', 'coffee', 'chocolate'])) {
-            $output->writeln('The drink type should be tea, coffee or chocolate.');
-        } else {
-            /**
-             * Tea       --> 0.4
-             * Coffee    --> 0.5
-             * Chocolate --> 0.6
-             */
-            $money = $input->getArgument('money');
-            switch ($drinkType) {
-                case 'tea':
-                    if ($money < 0.4) {
-                        $output->writeln('The tea costs 0.4.');
-                        return 0;
-                    }
-                    break;
-                case 'coffee':
-                    if ($money < 0.5) {
-                        $output->writeln('The coffee costs 0.5.');
-                        return 0;
-                    }
-                    break;
-                case 'chocolate':
-                    if ($money < 0.6) {
-                        $output->writeln('The chocolate costs 0.6.');
-                        return 0;
-                    }
-                    break;
-            }
+        $orderDrinkService = new OrderDrink();
 
-            $sugars = $input->getArgument('sugars');
-            $stick = false;
-            $extraHot = $input->getOption('extra-hot');
-            if ($sugars >= 0 && $sugars <= 2) {
-                $output->write('You have ordered a ' . $drinkType);
-                if ($extraHot) {
-                    $output->write(' extra hot');
-                }
+        $orderMsg = $orderDrinkService(
+            new OrderDrinkRequest(
+                $input->getArgument('drink-type'),
+                $input->getArgument('money'),
+                $input->getArgument('sugars'),
+                $input->getOption('extra-hot')
+            )
+        );
 
-                if ($sugars > 0) {
-                    $stick = true;
-                    $output->write(' with ' . $sugars . ' sugars (stick included)');
-                }
-                $output->writeln('');
-            } else {
-                $output->writeln('The number of sugars should be between 0 and 2.');
-            }
-        }
+        $output->writeln($orderMsg);
 
         return 0;
     }
